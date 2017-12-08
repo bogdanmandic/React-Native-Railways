69d7eeb93ced54dcf8705a7f070c69a06dff2b84
Dusan Zivkovic <dusan.zivkovic@cadev.rs>
2017-12-08 16:25:03 +0100

    about data

--- App.js
+++ App.js
@@ -203,26 +203,27 @@ export default class App extends Component {
         }
         NetInfo.getConnectionInfo()
           .then((res) => {
-            let warningString = res.type == 'cellular' ? 'Warning, you are on cellular network, this download could be charged.' : '';
+            let cellularType = res.effectiveType;
+            let warningString = res.type == 'cellular' ? 'Warning, you are on cellular ' + cellularType + ' network, this download could be charged.' : '';
             let downloadSpeed = 0;
-            switch (res.effectiveType) {
-              case '2g':
-                downloadSpeed = 0.04/8.0;
-                break;
-              case '3g':
-                downloadSpeed = 6.04/8.0;
-                break;
-              case '4g':
-                downloadSpeed = 18.4/8.0;
-                break;
-
-              default:
-                break;
-            }
-            let est = downloadSpeed != 0 ? (mb / downloadSpeed / 60).toFixed(1) : 'inf.';
+            if (res.type == 'cellular')
+              switch (res.effectiveType) {
+                case '2g':
+                  downloadSpeed = 0.04 / 8.0;
+                  break;
+                case '3g':
+                  downloadSpeed = 6.04 / 8.0;
+                  break;
+                case '4g':
+                  downloadSpeed = 18.4 / 8.0;
+                  break;
+              }
+            if (res.type == 'wifi')
+              downloadSpeed = 23.5 / 8.0;
+            let est = downloadSpeed != 0 ? (mb / downloadSpeed / 60).toFixed(0) + ' minutes ' + ((mb / downloadSpeed).toFixed(0) % 60) + ' seconds' : 'inf.';
             Alert.alert(
               'About to download ' + mb + ' MB',
-              '' + warningString + '\n' + 'Estimated time: ' + est + ' minutes.\nDo you wish to download?',
+              '' + warningString + '\n' + 'Estimated time: ' + est + '.\nDo you wish to download?',
               [{ text: 'OK', onPress: () => resolve() }, { text: 'Skip', onPress: () => reject() }]
             )
           })
